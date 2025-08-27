5c123e38d1a450315eef5c1ba212a98a42d796a5
satishgirigoudar <satishgirigoudar516@gmail.com>
2025-08-27 21:39:11 +0530

    basic vue App

---
+++ vue-project/src/App.vue
@@ -0,0 +1,4 @@
+
+<template>
+  <h1>welcome vue</h1>
+</template>
---
+++ vue-project/src/components/counter/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Counter</h2>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="decrment">Decrment</button>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 100
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrment() {
+      this.count > 0 ? this.count-- : this.count = 0
+    },
+    reset() {
+      this.count = 0
+    }
+  }
+
+};
+</script>
